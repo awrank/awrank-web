d3e7bf65a700581e5247df860294eee2b6fa681f
a-polyakov <qazsedcft1>
2013-02-08 20:14:09 +0200

    update UI localize login, forget password need    run awrank-web\aw-tools\src\main\java\com\awrank\web\tools\init\dictionary\InitDictionary.java

--- aw-backend/src/main/webapp/resources/js/awrank.js
+++ aw-backend/src/main/webapp/resources/js/awrank.js
@@ -1,53 +1,10 @@
 var contextPath = null;
 
-var l_dic_language = null;
-var l_dic_all = null;
-var l_dic = null;
-
-var oldPost = {};
-
-function setLanguage(language_code) {
-    l_dic_language = language_code;
-    l_dic = l_dic_all[l_dic_language];
-}
-
 function setContextPath(newContextPath) {
     contextPath = newContextPath + '/api/';
 }
 
-function getMessage(text) {
-    if (l_dic_all == undefined) {
-        $.ajax(
-            contextPath + 'dictionary/list', {
-                type: 'post',
-                async: false,
-                dataType: 'json',
-                success: function (data) {
-                    var list = data.dictionaryList;
-                    l_dic_all = {};
-                    for (var i = 0; i < list.length; i++) {
-                        var item = list[i];
-                        var l_dic_all_item = l_dic_all[item.language_code];
-                        if (l_dic_all_item == undefined) {
-                            l_dic_all_item = {};
-                            l_dic_all[item.language_code] = l_dic_all_item;
-                        }
-                        l_dic_all_item[item.code] = item.text;
-                    }
-                    setLanguage('RU');
-                },
-                error: function (errorData, textStatus) {
-                    alert('Error init dictionary');
-                }
-            });
-    }
-    var m = undefined;
-    if(l_dic && l_dic != undefined) m = l_dic[text];
-    if (m == undefined) {
-        m = text;
-    }
-    return m;
-}
+var oldPost = [];
 
 function postJson(url, data, success, disableElementId) {
     loadingShow(disableElementId);
@@ -55,46 +12,46 @@ function postJson(url, data, success, disableElementId) {
     if (data != null)
         dataJson = $.toJSON(data);
     $.ajax({
-    		url: contextPath + url,
-            type: "POST",
-            data: dataJson,
-            dataType: "json",
-            contentType: "application/json",
-            success: success,
-            error: function (errorData, textStatus) {
-                switch (errorData.status) {
-                    case 401:
-                        oldPost.push({url: url, data: data, success: success, disableElementId: disableElementId});
-                        //TODO
-                        alert(getMessage('ERROR') + '401');
+        url: contextPath + url,
+        type: "POST",
+        data: dataJson,
+        dataType: "json",
+        contentType: "application/json",
+        success: success,
+        error: function (errorData, textStatus) {
+            switch (errorData.status) {
+                case 401:
+                    oldPost.push({url: url, data: data, success: success, disableElementId: disableElementId});
+                    //TODO
+                    alert(getMessage('ERROR') + '401');
 //                        loginShow();
 
-                        break;
-                    case 403:
-                        alert(getMessage('ERROR') + ': ' + getMessage('ERROR_ACCESS'));
-                        break;
-                    case 500:
-                        var text = errorData.responseText
-                        if (text != null) {
-                            try {
-                                var o1 = $.evalJSON(text);
-                                if (o1 != null && o1.error != null && o1.error.message != null) {
-                                    text = getMessage(o1.error.message);
-                                }
-                            } catch (e) {
+                    break;
+                case 403:
+                    alert(getMessage('ERROR') + ': ' + getMessage('ERROR_ACCESS'));
+                    break;
+                case 500:
+                    var text = errorData.responseText
+                    if (text != null) {
+                        try {
+                            var o1 = $.evalJSON(text);
+                            if (o1 != null && o1.error != null && o1.error.message != null) {
+                                text = getMessage(o1.error.message);
                             }
+                        } catch (e) {
                         }
-                        alert(getMessage('ERROR') + ': ' + text);
-                        break;
-                    default:
-                        alert(getMessage('ERROR') + ': ' + errorData.status + " : "+getMessage('ERROR_NETWORK'));
-                        break;
-                }
-            },
-            complete: function () {
-                loadingHide();
+                    }
+                    alert(getMessage('ERROR') + ': ' + text);
+                    break;
+                default:
+                    alert(getMessage('ERROR') + ': ' + errorData.status + " : " + getMessage('ERROR_NETWORK'));
+                    break;
             }
-        });
+        },
+        complete: function () {
+            loadingHide();
+        }
+    });
 }
 
 var loadingIndex = 0;
