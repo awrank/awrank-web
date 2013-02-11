d2da1700410c90a3e84d079dc8a520d8bc9e6354
Andrew <andrew.v.stoyaltsev@gmail.com>
2013-02-12 00:00:40 +0200

    removed spring-social classes; minor refactoring web.xml and poms

--- aw-backend/src/main/java/com/awrank/web/backend/config/EnvironmentInitializer.java
+++ aw-backend/src/main/java/com/awrank/web/backend/config/EnvironmentInitializer.java
@@ -6,9 +6,11 @@ import org.springframework.core.env.ConfigurableEnvironment;
 
 public class EnvironmentInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
+	private static final String PROFILE_LOCALHOST = "localhost";
+
 	public void initialize(ConfigurableApplicationContext ctx) {
 		ConfigurableEnvironment environment = ctx.getEnvironment();
-		environment.setActiveProfiles("localhost");
+		environment.setActiveProfiles(PROFILE_LOCALHOST);
 	}
 
 }
