80b37daaf5c8431670618719da9c7e7b65904d34
Andrew <andrew.v.stoyaltsev@gmail.com>
2013-02-13 23:39:02 +0200

    separated profiles by folders; added boot properties file;

--- aw-backend/src/main/java/com/awrank/web/backend/config/EnvironmentInitializer.java
+++ aw-backend/src/main/java/com/awrank/web/backend/config/EnvironmentInitializer.java
@@ -1,27 +1,41 @@
 package com.awrank.web.backend.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.io.support.ResourcePropertySource;
 
+import java.io.IOException;
 
-//@PropertySource("/WEB-INF/properties/application.properties")
+/**
+ *
+ */
 public class EnvironmentInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-	private static final String PROFILE_LOCALHOST = "localhost";
-	private static final String PROFILE_PRODUCTION = "production";
+	private static Logger LOG = LoggerFactory.getLogger(EnvironmentInitializer.class);
 
 	public void initialize(ConfigurableApplicationContext ctx) {
 		ConfigurableEnvironment environment = ctx.getEnvironment();
-
-		Boolean isProduction = Boolean.parseBoolean(environment.getProperty("build.production.profile"));
-		if (isProduction) {
-			environment.setActiveProfiles(PROFILE_PRODUCTION);
-		} else {
-			environment.setActiveProfiles(PROFILE_LOCALHOST);
+		try {
+			environment.getPropertySources()
+					.addFirst(new ResourcePropertySource("classpath:props/boot.properties"));
+			LOG.info("props/boot.properties loaded");
+		} catch (IOException e) {
+			// it's ok if the file is not there. we will just log that info.
+			LOG.info("didn't find props/boot.properties in classpath so not loading it in the AppContextInitialized");
 		}
 
+		String property = environment.getProperty("build.production.profile");
+		Boolean isProduction = Boolean.parseBoolean(property);
+		System.out.println("build.production.profile=" + property);
+		if (isProduction) {
+			environment.setActiveProfiles(ProdConfiguration.PROFILE_PROD);
+		} else {
+			environment.setActiveProfiles(DevConfiguration.PROFILE_DEV);
+		}
+		//ctx.refresh();
 	}
 
 }
