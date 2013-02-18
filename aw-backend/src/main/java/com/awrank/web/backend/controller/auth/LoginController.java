bb7812b527565be031408bd7e39e16027fff5faf
andrew-st <andrew.v.stoyaltsev@gmail.com>
2013-02-18 20:15:49 +0200

    removed configs from POJO classes to xml files; profiles as well

--- aw-backend/src/main/java/com/awrank/web/backend/controller/auth/LoginController.java
+++ aw-backend/src/main/java/com/awrank/web/backend/controller/auth/LoginController.java
@@ -30,109 +30,109 @@ import java.util.List;
  */
 @Controller
 //@RequestMapping(value = "/auth")
-public class LoginController extends AbstractController{
+public class LoginController extends AbstractController {
 
-	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model, Principal principal) {
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, Principal principal) {
 
-		String name = principal.getName();
-		List<GrantedAuthority> authorities =
-				(List<GrantedAuthority>) ((UsernamePasswordAuthenticationToken) principal).getAuthorities();
+        String name = principal.getName();
+        List<GrantedAuthority> authorities =
+                (List<GrantedAuthority>) ((UsernamePasswordAuthenticationToken) principal).getAuthorities();
 
-		model.addAttribute("username", name);
-		model.addAttribute("authorities", authorities);
-		// define user role.
-		return "hello";
-	}
+        model.addAttribute("username", name);
+        model.addAttribute("authorities", authorities);
+        // define user role.
+        return "hello";
+    }
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(ModelMap model) {
-		return "login";
-	}
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(ModelMap model) {
+        return "login";
+    }
 
-	@RequestMapping(value = "/loginFailed", method = RequestMethod.GET)
-	public String loginFailed(ModelMap model) {
-		model.addAttribute("error", "true");
-		return "login";
-	}
+    @RequestMapping(value = "/loginFailed", method = RequestMethod.GET)
+    public String loginFailed(ModelMap model) {
+        model.addAttribute("error", "true");
+        return "login";
+    }
 
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public String logout(ModelMap model) {
-		SecurityContextHolder.clearContext();
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(ModelMap model) {
+        SecurityContextHolder.clearContext();
 
-		return "login";
-	}
+        return "login";
+    }
 
-	/* Social Auth */
-	@Autowired
-	private SocialAuthTemplate socialAuthTemplate;
+    /* Social Auth */
+    @Autowired
+    private SocialAuthTemplate socialAuthTemplate;
 
-	@RequestMapping(value = "/authSuccess")
-	public ModelAndView authSuccessPage(final HttpServletRequest request) throws Exception {
-		ModelAndView mv = new ModelAndView();
-		List<Contact> contactsList = new ArrayList<Contact>();
-		SocialAuthManager manager = socialAuthTemplate.getSocialAuthManager();
-		if (manager != null) {
-			AuthProvider provider = manager.getCurrentAuthProvider();
-			if (provider != null) {
-				contactsList = provider.getContactList();
-				if (contactsList != null && contactsList.size() > 0) {
-					for (Contact p : contactsList) {
-						if (!StringUtils.hasLength(p.getFirstName())
-								&& !StringUtils.hasLength(p.getLastName())) {
-							p.setFirstName(p.getDisplayName());
-						}
-					}
-				}
-				mv.addObject("profile", provider.getUserProfile());
-				mv.addObject("contacts", contactsList);
-			} else {
-				System.out.println("Provider is null!");
-			}
-		} else {
-			System.out.println("Manager is null!");
-		}
+    @RequestMapping(value = "/authSuccess")
+    public ModelAndView authSuccessPage(final HttpServletRequest request) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        List<Contact> contactsList = new ArrayList<Contact>();
+        SocialAuthManager manager = socialAuthTemplate.getSocialAuthManager();
+        if (manager != null) {
+            AuthProvider provider = manager.getCurrentAuthProvider();
+            if (provider != null) {
+                contactsList = provider.getContactList();
+                if (contactsList != null && contactsList.size() > 0) {
+                    for (Contact p : contactsList) {
+                        if (!StringUtils.hasLength(p.getFirstName())
+                                && !StringUtils.hasLength(p.getLastName())) {
+                            p.setFirstName(p.getDisplayName());
+                        }
+                    }
+                }
+                mv.addObject("profile", provider.getUserProfile());
+                mv.addObject("contacts", contactsList);
+            } else {
+                System.out.println("Provider is null!");
+            }
+        } else {
+            System.out.println("Manager is null!");
+        }
 
-		mv.setViewName("authSuccess");
-		return mv;
-	}
+        mv.setViewName("authSuccess");
+        return mv;
+    }
 
-	@RequestMapping(value = "/updateStatus", method = RequestMethod.POST)
-	public ModelAndView statusSuccessPage(final HttpServletRequest request)
-			throws Exception {
-		ModelAndView mv = new ModelAndView();
-		SocialAuthManager manager = socialAuthTemplate.getSocialAuthManager();
-		if (manager != null) {
-			AuthProvider provider = manager.getCurrentAuthProvider();
-			if (provider != null) {
-				String statusMsg = request.getParameter("statusMessage");
-				try {
-					provider.updateStatus(statusMsg);
-					mv.addObject("Message", "Status Updated successfully");
-				} catch (SocialAuthException e) {
-					mv.addObject("Message", e.getMessage());
-					e.printStackTrace();
-				}
-			} else {
-				mv.addObject("Message", "AuthProvider is null. Configuration has problems!");
-			}
+    @RequestMapping(value = "/updateStatus", method = RequestMethod.POST)
+    public ModelAndView statusSuccessPage(final HttpServletRequest request)
+            throws Exception {
+        ModelAndView mv = new ModelAndView();
+        SocialAuthManager manager = socialAuthTemplate.getSocialAuthManager();
+        if (manager != null) {
+            AuthProvider provider = manager.getCurrentAuthProvider();
+            if (provider != null) {
+                String statusMsg = request.getParameter("statusMessage");
+                try {
+                    provider.updateStatus(statusMsg);
+                    mv.addObject("Message", "Status Updated successfully");
+                } catch (SocialAuthException e) {
+                    mv.addObject("Message", e.getMessage());
+                    e.printStackTrace();
+                }
+            } else {
+                mv.addObject("Message", "AuthProvider is null. Configuration has problems!");
+            }
 
-		} else {
-			mv.addObject("Message", "SocialAuthManager is null. Configuration has problems!");
-		}
+        } else {
+            mv.addObject("Message", "SocialAuthManager is null. Configuration has problems!");
+        }
 
-		mv.setViewName("statusSuccess");
-		return mv;
-	}
+        mv.setViewName("statusSuccess");
+        return mv;
+    }
 
 //	@RequestMapping(value = "/socialauth")
 //	public void socialAuthCallback() {
 //		System.out.println("socialauth-callback");
 //	}
 
-	@RequestMapping(value = "/accessDenied")
-	public String accessDenied() {
-		return "accessDenied";
-	}
+    @RequestMapping(value = "/accessDenied")
+    public String accessDenied() {
+        return "accessDenied";
+    }
 
 }
