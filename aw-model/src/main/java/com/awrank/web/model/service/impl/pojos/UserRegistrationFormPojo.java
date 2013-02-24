488385eb7d627efc2274ce79b7f4d41dc9782796
Andrew <andrew.v.stoyaltsev@gmail.com>
2013-02-24 17:16:29 +0200

    corrected pojo name

--- aw-model/src/main/java/com/awrank/web/model/service/impl/pojos/UserRegistrationFormPojo.java
+++ aw-model/src/main/java/com/awrank/web/model/service/impl/pojos/UserRegistrationFormPojo.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  * @author Olga Korokhina
  */
 @SuppressWarnings("serial")
-public class UserRegistrationFormPOJO implements Serializable {
+public class UserRegistrationFormPojo implements Serializable {
 
     private String firstName;
 
